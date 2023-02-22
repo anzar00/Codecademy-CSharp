03655b0113b9558b0a36e389cdef78daf08f41fa
anzar000 <peerzada.anzar@gmail.com>
2023-02-22 23:35:49 +0530

    Getting Input

---
+++ Codecademy-CSharp/GettingInput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("How old are you?");
+                string input = Console.ReadLine();
+                Console.WriteLine($"You are {input} years old!");
+            }
+        }
+    }
+}
