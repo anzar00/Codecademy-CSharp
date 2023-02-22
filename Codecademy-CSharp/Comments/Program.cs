76035ff10979adef78515a9f1cd762283a03b013
anzar000 <peerzada.anzar@gmail.com>
2023-02-22 23:51:41 +0530

    Comments

---
+++ Codecademy-CSharp/Comments/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Ask the user to enter age
+                Console.WriteLine("How old are you?");
+                string input = Console.ReadLine();
+                Console.WriteLine($"You are {input} years old!");
+            }
+        }
+    }
+}
