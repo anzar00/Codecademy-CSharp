676bb5c9b76c8cf0aaad93d4e8934dc8eb22310b
anzar000 <peerzada.anzar@gmail.com>
2023-02-25 02:05:04 +0530

    Data Conversion

--- Codecademy-CSharp/ConvertingDataTypes/Program.cs
+++ Codecademy-CSharp/ConvertingDataTypes/Program.cs
@@ -10,6 +10,16 @@ namespace ConvertingDataTypes
     {
         static void Main(string[] args)
         {
+            // Ask user for fave number
+            Console.Write("Enter your favorite number!: ");
+
+            //int faveNumber = Console.ReadLine(); //Throws an error
+
+            //int faveNumber = (int)Console.ReadLine(); // Not possible to explicitly convert a string into an int (or vice versa) in C#
+
+            // Turn that answer into an int using built-in method
+
+            int faveNumber = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
